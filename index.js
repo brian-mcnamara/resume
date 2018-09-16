6d9372273559d6f2875d3d5c671807b2c353a8bc
Brian McNamara <brianmay27@gmail.com>
2018-09-16 13:55:30 -0700

    Add expandable content. Add some additional content

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+function expand(element) {
+  element.classList.add('expand');
+  element.parentElement.classList.add('expanded')
+}
 
+function collapse(element) {
+  element.classList.remove('expand');
+  element.parentElement.classList.remove('expanded')
+}
+
+function toggle() {
+  document.querySelectorAll('.expandable').forEach(elem => {
+    if (!expanded) {
+      expand(elem);
+    } else {
+      collapse(elem);
+    }
+  });
+  expanded = !expanded;
+  if (expanded) {
+    detachExpand();
+  } else {
+    attachExpand();
+  }
+}
+
+function attachExpand() {
+  document.querySelectorAll('.expandable').forEach(elem => {
+    elem.parentElement.onmouseover = () => { expand(elem) };
+    elem.parentElement.onmouseleave = () => { collapse(elem) };
+  });
+}
+
+function detachExpand() {
+  document.querySelectorAll('.expandable').forEach(elem => {
+    elem.parentElement.onmouseover = undefined;
+    elem.parentElement.onmouseleave = undefined;
+  });
+}
+
+var expanded = false;
+window.onload = function() {
+  attachExpand();
+  document.querySelectorAll('.expandable').forEach(elem => {
+    elem.parentElement.classList.add('hasExpandable')
+  })
+}
